f8828d423c197dc4b729ea961a4cee3e8032d0d5
vistafan12 <kroljestboss@o2.pl>
2020-04-13 17:34:42 +0200

    update after 3 years

--- resources/videos.sql
+++ resources/videos.sql
@@ -1,16 +1,29 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `email` varchar(15) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `register_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `videos` (
-  `id`         INT(11)                NOT NULL,
-  `hash`       VARCHAR(200)           NOT NULL,
-  `type`       TEXT                   NOT NULL,
-  `added`      DATETIME               NOT NULL,
-  `is_deleted` ENUM ('true', 'false') NOT NULL DEFAULT 'false'
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
+  `id` int(11) NOT NULL,
+  `hash` varchar(200) NOT NULL,
+  `type` text NOT NULL,
+  `added` datetime NOT NULL,
+  `user` varchar(11) DEFAULT NULL,
+  `title` varchar(140) DEFAULT NULL,
+  `is_deleted` enum('true','false') NOT NULL DEFAULT 'false'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 ALTER TABLE `videos`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `videos`
-  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 2;
+  ADD PRIMARY KEY (`id`),
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
